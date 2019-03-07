7fe30a5c7350b3f932e8759df42180e4b4429c33
xujian <782135@qq.com>
2019-03-08 00:33:59 +0800

    fix webpack build target config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   chainWebpack: config => {
-    config.optimization.delete('splitChunks')
-    config.entryPoints.delete('app')
-    config.entry('docs').add('./docs/main.ts')
-    config.entry('lib').clear()
-    config.entry('lib').add('./src/index.ts')
-  }
+  },
+  configureWebpack: {
+    output: {
+    },
+    externals: {
+      vue: 'Vue'
+    },
+    optimization: {
+      splitChunks: false
+    }
+  },
+  filenameHashing: false
 }
