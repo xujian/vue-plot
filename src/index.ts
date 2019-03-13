e33412d6999a62b4f1799bb9fd0f9621eea93835
xujian <782135@qq.com>
2019-03-13 17:41:13 +0800

    fix echarts canvas extra 25px height problem

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,48 @@ import __Vue__ from 'vue'
 import { charts, plugins } from './charts'
 import PaChart from './core/chart'
 import ChartFactory from './core/chart/Factory'
-import config, { setOptions } from './core/utils/config'
+import globalConfigs, { setGlobalConfigs } from './core/utils/configs'
+import { config } from '@vue/test-utils';
+
+function initGlobalObject(Vue: typeof __Vue__) {
+  let chartlib = new Vue({
+    computed: {
+      configs: {
+        get: () => globalConfigs,
+        set: function (configs: any) {
+          setGlobalConfigs(configs)
+          this.$emit('configs.changed', {
+            configs
+          })
+        }
+      },
+      theme: {
+        get: function() {
+          return globalConfigs.theme
+        },
+        set: function(theme: string) {
+          console.log('glogbleCOnfigs.----------///////set', theme)
+          globalConfigs.theme = theme
+          setGlobalConfigs(globalConfigs)
+          this.$bus.emit('theme.changed', {
+            theme: theme
+          })
+        }
+      }
+    }
+  })
+  Object.assign(Vue.prototype, {
+    $chartlib: chartlib
+  })
+}
 
 const Chartlib = {
-  install (Vue: typeof __Vue__, options = {}) {
-    setOptions(Object.assign(config, options))
+  install(Vue: typeof __Vue__, configs = {}) {
+    setGlobalConfigs(configs)
     for (let k in plugins) {
       Vue.use(plugins[k])
     }
-    Vue.prototype['$chartlib'] = {
-      setOptions (options) {
-        setOptions(Object.assign(config, options))
-      }
-    }
+    initGlobalObject(Vue)
   }
 }
 
