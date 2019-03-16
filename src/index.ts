32735be1c8ae168f74aaee86d6cb7c7211847da4
xujian <782135@qq.com>
2019-03-16 15:45:44 +0800

    + accessories

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import __Vue__ from 'vue'
 import { charts, plugins } from './charts'
+import * as accessories from './core/accessories'
 import PaChart from './core/chart'
 import ChartFactory from './core/chart/Factory'
 import globalConfigs, { setGlobalConfigs } from './core/utils/configs'
@@ -43,6 +44,10 @@ const Chartlib = {
     ThemeManager.registerPresetThemes()
     for (let k in plugins) {
       Vue.use(plugins[k])
+    }
+    for (let t in accessories) {
+      let a = accessories[t]
+      Vue.component(a.name, a)
     }
     initGlobalObject(Vue)
   }
