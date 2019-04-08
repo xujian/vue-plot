ea068ed324531d89d35bf0b956f9c00bd90e8970
xujian <782135@qq.com>
2019-04-08 17:44:41 +0800

    fix Bus.emit error

--- src/core/shared/events/bus.ts
+++ src/core/shared/events/bus.ts
@@ -2,16 +2,16 @@ import Vue from 'vue'
 
 const Bus = new Vue({
   methods: {
-    emit (event: string, ...args: any) {
-      this.$emit(event, ...args)
+    emit (event: string, args: any) {
+      this.$emit(event, args)
     },
-    on (event: string, ...args: any) {
+    on (event: string, args: any) {
       this.$on(event, args)
     },
-    off (event: string, ...args: any) {
+    off (event: string, args: any) {
       this.$off(event, args)
     },
-    once (event: string, ...args: any) {
+    once (event: string, args: any) {
       this.$once(event, args)
     }
   }
