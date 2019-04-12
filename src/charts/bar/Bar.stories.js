61bdcfb03c3b386884b7110fa189175ca64fa420
xujian <782135@qq.com>
2019-04-12 14:45:20 +0800

    + with dataset story

--- src/charts/bar/Bar.stories.js
+++ src/charts/bar/Bar.stories.js
@@ -99,11 +99,37 @@ states.forEach(s => {
   }))
 })
 
+stories.add('With dataset', () => ({
+  template: `<pa-bar-chart
+    :title="'年度GDP'"
+    :styles="{label:{rotate: 45, fontSize: 10}}"
+    :dataset="[
+      ['GDP', '福田区', '罗湖区', '盐田区',
+      '南山区', '宝安区', '龙华区', '光明区',
+      '龙岗区', '坪山区', '大鹏新区'],
+      ['2016', 3574.5612, 1976.2459, 540.2693,
+      3978.4758, 3071.2452, 1884.5820, 742.0341,
+      3470.4725, 530.7729, 311.0454],
+      ['2016', 3820.5692, 2161.6969, 585.9975,
+      4617.7993, 3453.1796, 2139.4243,
+      852.2589, 3871.1358, 607.9395, 331.2546]
+    ]">
+    </pa-bar-chart>`
+}))
+
 stories.add('With presets', () => ({
   template: `<pa-bar-chart
     :preset="'gdp-by-years'"
+    :styles="{
+      canvas: {
+        margin: ['5%', '8%', '20%', '8%']
+      },
+      label:{
+        rotate: 45,
+        fontSize: 10
+      }
+    }"
     :title="'年度GDP'"
-    :styles="{label:{rotate: 45, fontSize: 10}}"
     :dataset="'mocks:gdp-by-years-dataset'">
     </pa-bar-chart>`
 }))
