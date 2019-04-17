621a7d10aec7d7fd523a3502736a99b0b9d35b42
xujian <782135@qq.com>
2019-04-17 12:08:36 +0800

    fix props.x to options.xAxis problem

--- src/providers/echarts/options/props/x.ts
+++ src/providers/echarts/options/props/x.ts
@@ -1,30 +1,36 @@
 export default function (props: any) {
-  let x = props.x 
-  if (x === false) {
-    return {
+  let x = props.x
+  x = x || []
+  let xAxis: any = {
+    type: x.type || 'category'
+  }
+  if (xAxis.type === 'category') {
+    xAxis.data = x.data
+  }
+  if (x.label) {
+    xAxis.axisLabel = {
+      formatter: x.label
     }
   }
-  x = x || []
-  let x1: any = {}
-  if (x.constructor.name === 'Array') {
-    x1 = {
-      type: 'category',
-      data: x
+  if (Reflect.has(x, 'line')) {
+    let line = x.line
+    if (line === false) {
+      xAxis.axisLine = {
+        show: false
+      }
+    } else {
+
     }
-  } else {
-    x1 = {
-      type: x.type || 'category'
-    }
-    if (x.type === 'catetory') {
-      x1.data = x.data
-    }
-    if (x.label) {
-      x1.axisLabel = {
-        formatter: x.label
+  }
+  if (Reflect.has(x, 'ticks')) {
+    let ticks = x.ticks
+    if (ticks === false) {
+      xAxis.axisTick = {
+        show: false
       }
     }
   }
   return {
-    xAxis : [x1]
+    xAxis : [xAxis]
   }
 }
