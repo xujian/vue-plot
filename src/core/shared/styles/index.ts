22b4bcea45ba0373cc49eb9ff49f34bb3f251f93
xujian <782135@qq.com>
2019-04-18 19:12:17 +0800

    + StyleManager

--- src/core/shared/styles/index.ts
+++ src/core/shared/styles/index.ts
@@ -1,7 +1,28 @@
 import Styles, { StyleRules } from './Styles'
+import { Props } from '@/core/chart';
+
+let StyleManager = {
+  /**
+   * 处理props里已经layers里定义的styles属性
+   * 给styles编号以独立应用到各个sereies
+   * @param props 
+   */
+  make (props: Props): Styles[] {
+    let result: Styles[] = []
+    let styles: Styles = Styles.create(props.styles || {})
+    let StylesArray = Array(props.data.length).fill(styles)
+    result.push(...StylesArray)
+    props.layers.forEach((l: any) => {
+      let s: Styles = Styles.create(l.styles)
+      result.push(s)
+    })
+    return result
+  }
+}
 
 export {
-  StyleRules
+  StyleRules,
+  StyleManager
 }
 
 export default Styles
