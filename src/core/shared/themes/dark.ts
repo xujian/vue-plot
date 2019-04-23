6e9c4b1d214d8d6f29e17304986a279a5a0917e1
xujian <782135@qq.com>
2019-04-23 13:49:00 +0800

    trivial

--- src/core/shared/themes/dark.ts
+++ src/core/shared/themes/dark.ts
@@ -1,6 +1,6 @@
 export default {
   props: {
-    colors: ['#f845f1', '#ad46f3', '#5045f6', '#4777f5', '#44aff0', '#45dbf7', '#f6d54a', '#f69846', '#ff4343'],
+    colors: ['#5045f6', '#4777f5', '#44aff0', '#45dbf7', '#f6d54a', '#f69846', '#ff4343', '#f845f1', '#ad46f3'],
     background: '#1f2146',
     styles: {
       colors: [
