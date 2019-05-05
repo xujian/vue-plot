2075e233c42e97291d4eb9fb38e29797ccd7a6d9
xujian <782135@qq.com>
2019-05-06 01:28:36 +0800

    try to fix boolean prop item update value error

--- support/designtime/inspector/props/Boolean.vue
+++ support/designtime/inspector/props/Boolean.vue
@@ -7,28 +7,44 @@
       <q-toggle
         dark
         color="secondary"
-        :value="prop.__value"
-        @input="onInput"></q-toggle>
+        v-model="propValue"></q-toggle>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import PropInput from './PropInput'
+import { QToggle } from 'quasar'
 import { Component, Prop as PropDecorator } from 'vue-property-decorator'
 
 @Component({
-  name: 'PaString'
+  name: 'PaString',
+  components: {
+    QToggle
+  },
+  data () {
+    return {
+      propValue: false
+    }
+  },
+  watch: {
+    propValue (val: boolean) {
+      this.onInput(val)
+    }
+  }
 })
 export default class PaString extends PropInput {
-  onInput (value: string) {
+  
+  onInput (value: boolean) {
+    console.log('Boolean.vue_________________________////onIput', value)
     this.emitChange({
       ...this.prop,
-      value
+      value: value
     })
   }
 
   mounted () {
+    console.log('BOolean.vue-_______________________', this.prop)
   }
 }
 </script>
