1a28a4fab0fb9a00a205f06792365bb853614475
xujian <782135>
2019-05-21 01:02:28 +0800

    + eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,14 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 0,
+    'space-before-function-paren': 'always',
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/prefer-interface': 0
   },
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
