9948b54fc6746334c88e3cd5ad3eb0f33265132f
xujian <782135@qq.com>
2019-05-20 19:53:24 +0800

    + eslint and depends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
+  parser:  '@typescript-eslint/parser', 
+  extends: [
     'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
     '@vue/standard',
-    '@vue/typescript'
+    '@vue/typescript'  
+  ],
+  plugins: [
+    '@typescript-eslint'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
+  parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
   }
 }
