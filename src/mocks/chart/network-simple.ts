72fea16b1f5272aedfbf796bc990c9015f4d91ba
mgz <mgz@mingongze.local>
2019-06-06 11:50:15 +0800

    pa-network-chart 数据变成标准形式，与其他图表保持一致

--- src/mocks/chart/network-simple.ts
+++ src/mocks/chart/network-simple.ts
@@ -1,87 +1,30 @@
-import echarts from 'echarts'
 export default [
   [{
+    name: "数据中心",
+    value: [1000, 140],
+    symbolSize: 60,
+  },
+  {
     name: "平台1",
-    // category: 0,
-    // active: true,
+    category: 0,
     speed: '50kb/s',
-    value: [0, 0],
-    itemStyle: {
-      normal: {
-        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-          offset: 0,
-          color: '#157eff'
-        }, {
-          offset: 1,
-          color: '#35c2ff'
-        }]),
-      }
-    },
+    value: [0, 0]
   },
   {
     name: "平台2",
-    // category: 0,
-    // active: true,
+    category: 0,
     speed: '50kb/s',
     value: [0, 100],
-    itemStyle: {
-      normal: {
-        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-          offset: 0,
-          color: '#157eff'
-        }, {
-          offset: 1,
-          color: '#35c2ff'
-        }]),
-      }
-    },
   },
   {
     name: "平台3",
-    // category: 1,
+    category: 1,
     value: [0, 200],
-    itemStyle: {
-      normal: {
-        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-          offset: 0,
-          color: '#4B79A1'
-        }, {
-          offset: 1,
-          color: '#283E51'
-        }]),
-      }
-    },
   },
   {
     name: "平台4",
-    // category: 1,
-    value: [0, 300],
-    itemStyle: {
-      normal: {
-        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-          offset: 0,
-          color: '#4B79A1'
-        }, {
-          offset: 1,
-          color: '#283E51'
-        }]),
-      }
-    },
-  },
-  {
-    name: "数据中心",
-    value: [1000, 200],
-    symbolSize: 60,
-    itemStyle: {
-      normal: {
-        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-          offset: 0,
-          color: '#16BFFD'
-        }, {
-          offset: 1,
-          color: '#CB3066'
-        }]),
-      }
-    },
-  }]
+    category: 1,
+    value: [0, 300]
+  }
+  ]
 ]
