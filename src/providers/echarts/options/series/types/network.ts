3bd9fba9da7e7788ee68ad2ebfb7a9f2cc3f1207
mgz <mgz@mingongze.local>
2019-06-06 11:57:21 +0800

    pa-network-chart 增加edgeLabel, 样式由数据文件转到当前文件

--- src/providers/echarts/options/series/types/network.ts
+++ src/providers/echarts/options/series/types/network.ts
@@ -1,52 +1,57 @@
-const items: { [key: string]: any }[] = [{
-  name: "平台1",
-  // category: 0,
-  active: true,
-  speed: '50kb/s',
-  value: [0, 0]
-}, {
-  name: "平台2",
-  // category: 0,
-  active: true,
-  speed: '50kb/s',
-  value: [0, 100]
-}, {
-  name: "平台3",
-  // category: 1,
-  value: [0, 200]
-}, {
-  name: "平台4",
-  // category: 1,
-  value: [0, 300]
-}]
-const targetCoord = [1000, 140]
+import echarts from 'echarts'
+import data from '../../../../../../src/mocks/chart/network-simple'
 
-const item1: { [key: string]: any } = {
-  name: "数据中心",
-  value: targetCoord,
-  symbolSize: 100,
-  itemStyle: {
-    // normal: {
-    //   color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-    //     offset: 0,
-    //     color: '#157eff'
-    //   }, {
-    //     offset: 1,
-    //     color: '#35c2ff'
-    //   }]),
-    // }
+// 根据category 设置不同分类的样式，name 可以作为legend的name
+const categories = [
+  {
+    // name: ' 流入中',
+    itemStyle: {
+      normal: {
+        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+          offset: 0,
+          color: '#4B79A1'
+        }, {
+          offset: 1,
+          color: '#283E51'
+        }]),
+      }
+    },
   },
-  label: {
-    normal: {
-      fontSize: '14'
-    }
-  },
+  {
+    itemStyle: {
+      // name: '暂无流入',
+      normal: {
+        color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+          offset: 0,
+          color: '#157eff'
+        }, {
+          offset: 1,
+          color: '#35c2ff'
+        }]),
+      }
+    },
+  }
+]
+
+// 默认截取第一个数据作为被连接的点
+const targetDot: { [key: string]: any } = data[0][0]
+targetDot.itemStyle = {
+  normal: {
+    color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+      offset: 0,
+      color: '#16BFFD'
+    }, {
+      offset: 1,
+      color: '#CB3066'
+    }]),
+  }
 }
 
+// 剩余的数据作为原点(去连接的点)
+const length = data[0].length
+const sourceDot: { [key: string]: any }[] = data[0].slice(1, length)
 
 export default function (series: any[], props: any) {
-  console.log('props........,,', props)
-  debugger
   series.forEach((s: any, i: number) => {
     let item: any = {
       label: {
@@ -54,12 +59,20 @@ export default function (series: any[], props: any) {
           show: true
         }
       },
-      edgeSymbol: ['circle', 'arrow'],
+      edgeSymbol: ['none', 'arrow'],
       edgeSymbolSize: [4, 10],
       edgeLabel: {
         normal: {
+          show: true,
           textStyle: {
-            fontSize: 20
+            fontSize: 14
+          },
+          formatter(params: any) {
+            let txt = ''
+            if (params.data.speed !== undefined) {
+              txt = params.data.speed
+            }
+            return txt
           }
         }
       },
@@ -68,12 +81,13 @@ export default function (series: any[], props: any) {
         width: 2,
         curveness: 0
       },
+      categories: categories,
       layout: 'none',
       coordinateSystem: 'cartesian2d',
-      links: items.map(el => {
+      links: sourceDot.map(el => {
         return {
           source: el.name,
-          target: item1.name,
+          target: targetDot.name,
           speed: el.speed,
           lineStyle: {
             normal: {
